b0f28703da8d3bfdded0ee4fd8a278aec69d4af1
Adriaan Schmidt <adriaan.schmidt@siemens.com>
2025-10-10 11:49:36 +0200

    refactor: test execution logic

--- industrial_jitterdebugger/webui.py
+++ industrial_jitterdebugger/webui.py
@@ -3,7 +3,6 @@
 # See LICENSE file in the top-level directory.
 
 import os
-from os.path import isfile
 import time
 import json
 import requests
@@ -103,9 +102,9 @@ def calculate_duration(num, t_unit):
 
 
 def do_test(duration):
-    if isfile("/publish/jitter-results.txt"):
+    if os.path.isfile("/publish/jitter-results.txt"):
         os.remove("/publish/jitter-results.txt")
-    if isfile("/publish/stress-results.txt"):
+    if os.path.isfile("/publish/stress-results.txt"):
         os.remove("/publish/stress-results.txt")
     data = {"duration": duration}
     requests.post(f"{JITTER_SERVICE_URL}/runjitter", json=data, timeout=3)
@@ -153,21 +152,23 @@ def get_test_running_time():
     return result
 
 
-def show_test_results():
-    while True:
-        if isfile("/publish/jitter-results.txt") and isfile("/publish/stress-results.txt"):
-            jitter_res = read_json_file("/publish/jitter-results.txt")
+def show_test_results(header="Test Results"):
+    st.subheader(header)
+    if os.path.isfile("/publish/jitter-results.txt"):
+        jitter_res = read_json_file("/publish/jitter-results.txt")
+        l, r = st.columns(2)
+        with l:
+            st.image("/publish/jitter-histogram.png", caption="CPU Jitter Histogram")
+        with r:
+            st.image("/publish/jitter-cdf.png", caption="CPU Cumulative Distribution")
+        with st.expander("See jitterdebuger results: "):
+            st.json(jitter_res)
+        if os.path.isfile("/publish/stress-results.txt"):
             stress_res = read_yaml_file("/publish/stress-results.txt")
-            l, r = st.columns(2)
-            with l:
-                st.image("/publish/jitter-histogram.png", caption="CPU Jitter Histogram")
-            with r:
-                st.image("/publish/jitter-cdf.png", caption="CPU Cumulative Distribution")
-            with st.expander("See jitterdebuger results: "):
-                st.json(jitter_res)
             with st.expander("See stress-ng results: "):
                 st.json(stress_res)
-            break
+    else:
+        st.write("No results found.")
 
 
 def main():
@@ -178,8 +179,8 @@ def main():
         layout="wide"
     )
     st.markdown(STREAMLIT_STYLE, unsafe_allow_html=True)
-    _, center, _ = st.columns([1, 3, 1])
 
+    _, center, _ = st.columns([1, 3, 1])
     with center:
         st.image(asset_path("sie-logo-white-rgb.png"), width=150)
         st.title("Industrial Jitterdebugger")
@@ -198,54 +199,49 @@ def main():
             number = st.number_input("Enter test duration: ", value=3, format="%d", min_value=1,
                                      placeholder="Type a number...")
         with col2:
-            unit = st.selectbox(
-                    "option",
-                    ("seconds", "minutes", "hours"), index=1, label_visibility="hidden")
+            unit = st.selectbox("option", ("seconds", "minutes", "hours"), index=1, label_visibility="hidden")
         if "but_start" not in st.session_state:
             st.session_state.disabled = False
         start = st.button("START TEST", key="but_start", on_click=click_start, disabled=False)
 
-        if not is_test_running():
-            if start:
-                duration_seconds, duration_string = calculate_duration(number, unit)
-                do_test(duration_string)
-                st.write(f"The test will last for {number} {unit}")
-                PROGRESS_TEXT = "please wait..."
-                pbar = st.progress(0, text=PROGRESS_TEXT)
-                timer_interval = duration_seconds/100
-                for percent_complete in range(100):
-                    time.sleep(timer_interval)
-                    pbar.progress(percent_complete + 1, text=PROGRESS_TEXT)
-                PROGRESS_TEXT = "Done!"
-                pbar.progress(100, text=PROGRESS_TEXT)
-                st.success("Test finished successfully")
-                st.subheader("Check Results")
-                show_test_results()
-            if not start:
-                if isfile("/publish/jitter-results.txt") and isfile("/publish/stress-results.txt"):
-                    st.subheader("Results of Last Test")
-                    show_test_results()
+        running_seconds = 0
+        duration_seconds, duration_string = calculate_duration(number, unit)
+        PROGRESS_TEXT = "please wait..."
 
         if is_test_running():
             if start:
                 st.toast("Please avoid clicking on the button repeatedly, the test is already running", icon="⚠️")
             number, unit = get_test_total_time()
-            st.write(f"The test is already running and will last for {number} {unit}")
-            PROGRESS_TEXT = "please wait..."
             duration_seconds, _ = calculate_duration(number, unit)
             running_seconds = get_test_running_time()
-            curr_progress = int((running_seconds/duration_seconds)*100)
-            pbar = st.progress(curr_progress, text=PROGRESS_TEXT)
-            timer_interval = duration_seconds/100
-            for percent_complete in range(curr_progress, 100):
-                time.sleep(timer_interval)
-                pbar.progress(percent_complete + 1, text=PROGRESS_TEXT)
-            PROGRESS_TEXT = "Done!"
-            pbar.progress(100, text=PROGRESS_TEXT)
-            st.success("Test finished successfully")
-            st.subheader("Check the results")
-            show_test_results()
+            st.write("The test is already running and will last for ", number, unit)
+        elif start:
+            do_test(duration_string)
+        else:
+            show_test_results(header="Results of Previous Test")
+            st.markdown(FOOTER, unsafe_allow_html=True)
+            st.stop()
 
+        curr_progress = int((running_seconds/duration_seconds)*100)
+        pbar = st.progress(curr_progress, text=PROGRESS_TEXT)
+
+        while running_seconds < duration_seconds:
+            time.sleep(10)
+            running_seconds += 10
+            if not is_test_running():
+                break
+            curr_progress = int((running_seconds/duration_seconds)*100)
+            pbar.progress(curr_progress, text=PROGRESS_TEXT)
+
+        if running_seconds >= duration_seconds:
+            PROGRESS_TEXT = "Done!"
+            curr_progress = 100
+        else:
+            requests.post(f"{STRESS_SERVICE_URL}/killstress", timeout=3)
+            PROGRESS_TEXT = "Test dead"
+        pbar.progress(curr_progress, text=PROGRESS_TEXT)
+        time.sleep(3)
+        show_test_results()
         st.markdown(FOOTER, unsafe_allow_html=True)
 
 
